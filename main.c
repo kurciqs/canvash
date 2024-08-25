ff52f181680699adb00e41c75bfbbb2c73aef6cc
kurciqs <kurciqs@gmail.com>
2024-08-25 21:27:54 +0200

    i can do the grid(dy) now

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #define t canvash_time()
 #define RAND_COLOR (vec4){(float)(rand()%255)/255.0f,(float)(rand()%255)/255.0f,(float)(rand()%255)/255.0f, 1.0f}
 #define RGB_COLOR(x,y,z) (vec4){(float)(x)/255.0f,(float)(y)/255.0f,(float)(z)/255.0f, 1.0f}
-
+#define GRID_AT(i,j) ((i) * grid_size[1] + (j))
 
 int main()
 {
@@ -16,10 +16,10 @@ int main()
     vec4 colors_by_index[3];
 
     glm_vec4_copy(RGB_COLOR(0, 0, 0), colors_by_index[0]);
-    glm_vec4_copy(RGB_COLOR(255, 0, 0), colors_by_index[1]);
+    glm_vec4_copy(RGB_COLOR(255, 255, 0), colors_by_index[1]);
     glm_vec4_copy(RGB_COLOR(0, 255, 0), colors_by_index[2]);
 
-    int dimensions = 40;
+    int dimensions = 10;
     float width, height;
     canvash_get_window_size(&width, &height);
 
@@ -31,24 +31,57 @@ int main()
     {
         for (int j = 0; j < grid_size[1]; ++j)
         {
-            grid[i*grid_size[1]+j] = 0;
+            grid[GRID_AT(i,j)] = 0;
         }
     }
 
+    grid[GRID_AT(0,0)] = 1;
+
     // NOTE this is your main loop, it's like the draw() function in p5.js
+
+    int frame = 0;
+
     while (canvash_running())
     {
         // NOTE essential (do not leave out)
         canvash_clear_screen();
         canvash_background((vec3){0.2f, 0.3f, canvash_time()/200.0f});
 
+        if (frame >= 60)
+        {
+            int* new_grid = (int*)malloc(sizeof(int) * grid_size[0] * grid_size[1]);
+            memcpy(new_grid, grid, sizeof(int) * grid_size[0] * grid_size[1]);
+
+            for (int i = 0; i < grid_size[0]; ++i)
+            {
+                for (int j = 0; j < grid_size[1]; ++j)
+                {
+                    const int id = grid[GRID_AT(i,j)];
+                    printf("%d %d %d\n", id, i+1,grid_size[0]);
+                    if (id != 0 && i + 1 < grid_size[0] && j + 1 < grid_size[1])
+                    {
+                        new_grid[GRID_AT(i,j)] = 0;
+                        new_grid[GRID_AT(i+1,j)] = 1;
+                        new_grid[GRID_AT(i,j+1)] = 1;
+                    }
+                }
+            }
+
+            memcpy(grid, new_grid, sizeof(int) * grid_size[0] * grid_size[1]);
+            free((void*)new_grid);
+
+            frame = 0;
+        }
+
+
         for (int i = 0; i < grid_size[0]; ++i)
         {
             for (int j = 0; j < grid_size[1]; ++j)
             {
-                const int id = grid[i*grid_size[1]+j];
+                const int id = grid[GRID_AT(i,j)];
 
                 canvash_fill_color(colors_by_index[id]);
+                // canvash_fill_color(RGB_COLOR(GRID_AT(i,j) * 10, GRID_AT(i,j) * 10, GRID_AT(i,j) * 10));
                 canvash_no_stroke();
                 canvash_rectangle_2D((vec2){i*cell_size[0] - width/2.0f, j*cell_size[1] - height/2.0f}, (vec2){(i+1)*cell_size[0]- width/2.0f, (j+1)*cell_size[1] - height/2.0f});
             }
@@ -71,13 +104,16 @@ int main()
                 i = (int)((float)(pos_x)/(cell_size[0]));
                 j = (int)((float)(pos_y)/(cell_size[1]));
 
-                const int index = i*grid_size[1]+j;
+                const int index = GRID_AT(i,j);
                 grid[index] = 1;
+
+                printf("press: %d %d %d\n", i, j, index);
             }
         }
 
         // NOTE essential (do not leave out)
         canvash_render();
+        frame++;
     }
 
     free((void*)grid);
